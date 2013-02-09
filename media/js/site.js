c18b9fb2c34fa4affc2882d8ae343a42507f6d44
Ryan Roemer <ryan@loose-bits.com>
2013-02-09 08:04:06 -0500

    Starting work on new navbar.

--- media/js/site.js
+++ media/js/site.js
@@ -1,54 +1,56 @@
-$(function () {
-  var _navClicked = false,
-    defaultWidth = 700,
-    lastWidth = defaultWidth;
 
-  // Handle open/close responsive to default bug.
-  $(window).resize(function() {
-    var newWidth = $(window).width();
+// TODO REMOVE
+// $(function () {
+//   var _navClicked = false,
+//     defaultWidth = 700,
+//     lastWidth = defaultWidth;
 
-    if (lastWidth < newWidth &&
-        lastWidth < defaultWidth &&
-        defaultWidth < newWidth) {
+//   // Handle open/close responsive to default bug.
+//   $(window).resize(function() {
+//     var newWidth = $(window).width();
 
-      // Crossed threshold getting larger.
-      // Manually reset to starting point.
-      $('.btn.btn-navbar')
-        .removeClass("collapsed");
+//     if (lastWidth < newWidth &&
+//         lastWidth < defaultWidth &&
+//         defaultWidth < newWidth) {
 
-      $('.nav-collapse')
-        .removeClass("collapse")
-        .removeClass("in")
-        .css("height", "auto")
-        .show();
+//       // Crossed threshold getting larger.
+//       // Manually reset to starting point.
+//       $('.btn.btn-navbar')
+//         .removeClass("collapsed");
 
-      _navClicked = false;
+//       $('.nav-collapse')
+//         .removeClass("collapse")
+//         .removeClass("in")
+//         .css("height", "auto")
+//         .show();
 
-    } else if (newWidth < lastWidth &&
-               newWidth < defaultWidth &&
-               defaultWidth < lastWidth) {
+//       _navClicked = false;
 
-      // Crossed threshold getting smaller.
-      $('.nav-collapse').hide();
-      _navClicked = false;
-   }
+//     } else if (newWidth < lastWidth &&
+//                newWidth < defaultWidth &&
+//                defaultWidth < lastWidth) {
 
-    lastWidth = newWidth;
-  });
+//       // Crossed threshold getting smaller.
+//       $('.nav-collapse').hide();
+//       _navClicked = false;
+//    }
 
-  // Fix collapse to properly open / close.
-  $('.btn.btn-navbar').on('click', function () {
-    // First time doesn't open correctly. Manually do this.
-    // See: https://github.com/twitter/bootstrap/issues/3184
-    if (!_navClicked) {
-      _navClicked = true;
-      $('.nav-collapse').show();
-      return;
-    }
-    $('.nav-collapse').toggle();
-  });
+//     lastWidth = newWidth;
+//   });
 
-  $('.nav-collapse a[data-toggle]').click(function () {
-    $('.nav-collapse').css('height', '100%');
-  });
-});
+//   // Fix collapse to properly open / close.
+//   $('.btn.btn-navbar').on('click', function () {
+//     // First time doesn't open correctly. Manually do this.
+//     // See: https://github.com/twitter/bootstrap/issues/3184
+//     if (!_navClicked) {
+//       _navClicked = true;
+//       $('.nav-collapse').show();
+//       return;
+//     }
+//     $('.nav-collapse').toggle();
+//   });
+
+//   $('.nav-collapse a[data-toggle]').click(function () {
+//     $('.nav-collapse').css('height', '100%');
+//   });
+// });
